28f15f734ca08d766464a860ac997073d06dfb02
evanchooly <jlee@antwerkz.com>
2016-08-09 21:29:20 -0600

    build trigger

--- src/main/kotlin/javabot/javadoc/JavadocParser.kt
+++ src/main/kotlin/javabot/javadoc/JavadocParser.kt
@@ -57,7 +57,6 @@ class JavadocParser @Inject constructor(val apiDao: ApiDao, val javadocClassDao:
                 }
 
 //                buildHtml(api, location, packages)
-
                 println("Waiting for queue to drain")
                 Awaitility
                     .waitAtMost(30, MINUTES)
