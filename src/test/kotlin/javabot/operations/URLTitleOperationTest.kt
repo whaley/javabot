602d18bb704bb9d384235f65b4cdd69efa4347f2
Jason Whaley <jasonwhaley@gmail.com>
2018-07-23 09:40:24 -0400

    Test w/ jetty: fails localhost blacklisted
    Dynamic blacklisting is probably a prerequisite to doing this properly

--- src/test/kotlin/javabot/operations/URLTitleOperationTest.kt
+++ src/test/kotlin/javabot/operations/URLTitleOperationTest.kt
@@ -5,8 +5,8 @@ import javabot.Message
 import javabot.operations.urlcontent.URLContentAnalyzer
 import org.eclipse.jetty.server.Request
 import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.server.ServerConnector
 import org.eclipse.jetty.server.handler.AbstractHandler
-import org.jsoup.Jsoup
 import org.testng.Assert
 import org.testng.Assert.assertEquals
 import org.testng.annotations.AfterClass
@@ -24,13 +24,26 @@ class URLTitleOperationTest : BaseTest() {
     private val analyzer = URLContentAnalyzer()
 
     private var server: Server? = null
+    private var port = 0
+    private val pathsToTitles = mapOf(
+            "/" to "Google",
+            "/java-channel" to "Freenode ##java  enthusiasts united",
+            "/finding-hash-collisions-in-java-strings" to "Finding hash collisions in Java Strings",
+            "/javase/tutorial/java/nutsandbolts/branch.html" to "Branching Statements (The Java Tutorials > Learning the Java Language > Language Basics",
+            "/non-ascii-title-here" to "使用百度前必读 意见反馈 non-ascii-title-here 使用百度前必读 意见反馈",
+            "/articles/why-programmers-should-have" to "Why Programmers Should Have a Blog - DZone Agile",
+            "/javaee/7/api/javax/enterprise/inject/Instance.html" to "Instance (Java(TM) EE 7 Specification APIs)"
+    )
 
     @BeforeClass
     fun startEmbeddedJetty() {
-        val server = Server(9999)
-        server.handler = HandlerForTitleTests(mapOf(("/foobar" to "vegan food")))
+        val server = Server(0)
+        server.handler = HandlerForTitleTests(pathsToTitles)
         server.start()
         server.dumpStdErr()
+
+        val connector = server.connectors.first() as ServerConnector
+        port = connector.localPort
     }
 
     @AfterClass
@@ -38,36 +51,26 @@ class URLTitleOperationTest : BaseTest() {
         server?.stop()
     }
 
-    class HandlerForTitleTests(val pathsToTitles: Map<String,String>) : AbstractHandler() {
+    class HandlerForTitleTests(private val pathsToTitles: Map<String,String>) : AbstractHandler() {
         override fun handle(target: String, baseRequest: Request, request: HttpServletRequest, response: HttpServletResponse) {
+            val title = pathsToTitles[request.pathInfo]
             response.contentType = "text/html; charset=utf-8"
-            response.status = HttpServletResponse.SC_OK
+            response.status = if (title == null) HttpServletResponse.SC_NOT_FOUND else HttpServletResponse.SC_OK
 
             val out = response.writer
-            val title = pathsToTitles.getOrDefault(request.pathInfo, "Request was to a path we aren't testing for, you insolent clod")
             out.println("<title>$title</title>")
             baseRequest.isHandled = true
         }
-    }
-    
-    @Test
-    fun testEmbeddedJetty() {
-        val document = Jsoup
-                .connect("http://localhost:9999/foobar")
-                .userAgent("Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/41.0")
-                .timeout(5000)
-                .get()
-        Assert.assertEquals(document.title(), "vegan food")
-
     }
 
     @Test(dataProvider = "urls")
     fun testSimpleUrl(url: String, content: String?) {
         val results = operation.handleChannelMessage(Message(TEST_CHANNEL, TEST_USER, url))
         if (content != null) {
-            Assert.assertEquals(results[0].value, content)
+            val result = if (results.isNotEmpty()) results[0].value else ""
+            Assert.assertEquals(result, content,  "Results for '$url' should [$content]")
         } else {
-            Assert.assertTrue(results.isEmpty(), "Results for '$url' should be empty: $results")
+            Assert.assertTrue(results.isEmpty(), "Results for '$url' should be empty: was [$results]")
         }
     }
 
@@ -78,41 +81,40 @@ class URLTitleOperationTest : BaseTest() {
 
     @DataProvider(name = "urls")
     fun getUrls(): Array<Array<*>> {
+        val baseUrl = "http://localhost:$port"
         return arrayOf(
-                arrayOf("http://google.com/", null),
-                arrayOf("http://google.com", null),
-                arrayOf("http://localhost", null),
-                arrayOf("http://127.0.0.1", null),
-                arrayOf("http://a", null), // should work unless DNS gets in the way, as it does for me
-                arrayOf("Have you tried to http://google.com", null),
-                arrayOf("http://varietyofsound.wordpress.com has a lot of VSTs", null),
-                arrayOf("Have you tried to http://javachannel.org/", "botuser's title: \"Freenode ##java  enthusiasts united\""),
-                arrayOf("http://javachannel.org/posts/finding-hash-collisions-in-java-strings/", null),
-                arrayOf("http://hastebin.com/askhjahs", null),
-                arrayOf("http://pastebin.com/askhjahs", null),
-                arrayOf("http://architects.dzone.com/articles/why-programmers-should-have",
+                arrayOf(baseUrl, null),
+                arrayOf("Some text before $baseUrl", null),
+                arrayOf("Some text before $baseUrl/java-channel", "botuser's title: \"Freenode ##java  enthusiasts united\""),
+                arrayOf("$baseUrl some text after", null),
+                arrayOf("$baseUrl/java-channel some text after", "botuser's title: \"Freenode ##java  enthusiasts united\""),
+                arrayOf("$baseUrl/finding-hash-collisions-in-java-strings", null),
+                arrayOf("$baseUrl/articles/why-programmers-should-have",
                         "botuser's title: \"Why Programmers Should Have a Blog - DZone Agile\""), // url matches title
-                arrayOf("http://facebook.com/foo/bar/blah", null), // doesn't exist on facebook, I hope
+                arrayOf("$baseUrl/doesnt_exist", null),
                 arrayOf("http://", null),
-                arrayOf("http://docs.oracle.com/javaee/6/tutorial/doc/", "botuser's title: \"- The Java EE 6 Tutorial\""),
-                arrayOf("https://docs.oracle.com/javaee/7/api/javax/enterprise/inject/Instance.html", null),
-                arrayOf("http://docs.oracle.com/javase/tutorial/java/nutsandbolts/branch.html",
+                arrayOf("http://a", null),
+                arrayOf("$baseUrl/javase/tutorial/java/nutsandbolts/branch.html",
                         "botuser's title: \"Branching Statements (The Java Tutorials > Learning the Java Language > Language Basics)\""),
-                arrayOf("http://git.io/foo", null),
-                arrayOf("Two urls with titles: http://docs.oracle.com/javaee/6/tutorial/doc/ and http://javachannel.org/",
+                arrayOf("$baseUrl/javaee/7/api/javax/enterprise/inject/Instance.html", null),
+
+                arrayOf("Two urls with titles: $baseUrl/finding-hash-collisions-in-java-strings and $baseUrl/java-channel",
                         "botuser's titles: \"- The Java EE 6 Tutorial\" | \"Freenode ##java  enthusiasts united\""),
-                arrayOf("Two urls, one with a title: http://javachannel.org/posts/finding-hash-collisions-in-java-strings/  and " +
-                        "http://javachannel.org/", "botuser's title: \"Freenode ##java  enthusiasts united\""),
-                arrayOf("Two urls, duplicated:  http://javachannel.org/ and http://javachannel.org/"
+                arrayOf("Two urls, one with a title: $baseUrl/finding-hash-collisions-in-java-strings and $baseUrl/java-channel",
+                        "botuser's title: \"Freenode ##java  enthusiasts united\""),
+                arrayOf("Two urls, duplicated:  $baseUrl/java-channel and $baseUrl/java-channel"
                         , "botuser's title: \"Freenode ##java  enthusiasts united\""),
-                arrayOf("https://twitter.com/djspiewak/status/1004038775989678080", "botuser's title: \"Daniel Spiewak on Twitter: \"Random best practice note: just because your language has type inference doesn't mean it's bad to explicitly write types. Types are good! Types are documentation. Don't make future code reviewers play the human compiler game.\"\""),
-                arrayOf("http://refheap.com", null), // this may change: right now, refheap.com returns a 502
-                arrayOf("Ignore title if it doesn't contain at least 20 ascii chars https://www.baidu.com/", null)
+
+                arrayOf("Ignore title if it doesn't contain at least 20 ascii chars $baseUrl/non-ascii-title-here", null)
         )
     }
 
     @DataProvider(name = "urlRulesCheck")
     fun getUrlsForRulesCheck(): Array<Array<*>> {
+        //TODO check that 127.0.0.1 is blacklisted
+        //TODO should hastebin and patebin be blacklisted?
+        //                arrayOf("http://hastebin.com/askhjahs", null),
+        //                arrayOf("http://pastebin.com/askhjahs", null),
         return arrayOf(arrayOf("http://pastebin.com", "pastebin for your wastebin", false),
                 arrayOf("http://makemoneyfast.com/super-profit", "make money fast! super profit", false),
                 arrayOf("http://varietyofsound.wordpress.com", "Variety Of Sound", false),
